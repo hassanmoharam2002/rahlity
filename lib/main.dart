da351bc7a5ab77d476423d687f4113a2d8d60e29
hassanmo <hassanmoharam2002@gmail.com>
2024-02-21 08:13:06 +0200

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:home/aurh/login.dart';
 
 import 'aurh/signup.dart';
